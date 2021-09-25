470730e4d13c066df430282b181f367d28ce621c
K1nGsmaN-hub <15162342h@gmail.com>
2021-09-25 12:03:27 +0800

    feat: initialize project

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const App: FC = () => {
+    return (
+        <Router>
+            <>Hello</>
+        </Router>
+    );
+};
+
+export default App;
