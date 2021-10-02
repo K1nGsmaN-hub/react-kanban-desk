4d2d200a3ae840247904fec05db8cdb7c5e24cf1
K1nGsmaN-hub <15162342h@gmail.com>
2021-10-02 19:32:34 +0800

    feat: edit api urls

--- server/api/urls.js
+++ server/api/urls.js
@@ -1,48 +1,20 @@
-export const URLS = {
-    MAIN: '/',
-
+module.exports = {
     // Users
-    USER: `/users/`, // get, post
-    USER_BY_ID: `/users/:id`, // delete, get, patch
+    USERS: `/api/users`, // post
+    USER_BY_ID: `/api/users/:user_id`, // delete, get, patch
 
     // Tables
-    TABLES: `${this.USER_BY_ID}/tables`, // get, post
-    TABLE_BY_ID: `${this.TABLES}/:table_id`, // delete, get, patch
+    TABLES: `/api/tables`, // post
+    TABLE_BY_ID: `/api/tables/:table_id`, // delete, patch
+    TABLES_BY_USER_ID: '/api/tables/?user_id=', // get
 
     // Columns
-    COLUMNS: `${this.TABLE_BY_ID}/columns`, // get, post
-    COLUMN_BY_ID: `${this.COLUMNS}/:column_id`, // delete, patch
+    COLUMNS: `/api/columns`, // post
+    COLUMN_BY_ID: `/api/columns/:column_id`, // delete, patch
+    COLUMNS_BY_TABLE_ID: '/api/columns/?table_id=', // get
 
     // Tasks
-    TASKS: `${this.COLUMN_BY_ID}/tasks`, // post
-    TASK_BY_ID: `${this.TASKS}/:task_id`, // delete, get, patch
+    TASKS: `/api/tasks`, // post
+    TASK_BY_ID: `/api/tasks/:task_id`, // delete, get, patch
+    TASKS_BY_TABLE_ID: '/api/tasks/?table_id=', // get
 };
-
-/*
-[GET] /api/ - все данные из БД <DBStructor>
-
-[GET] /api/users/ - все пользователи <User[]>
-[POST] /api/users/ добавить нового пользователя <User>
-
-[GET] /api/users/:id/ - пользователь по ID <User>
-[PATCH] /api/users/:id/ - изменение параметров пользователя
-[DELETE] /api/users/:id/ - удалить пользователя
-
-[GET] /api/users/:id/tables/ - все таблицы пользователя <Table[]>
-[POST] /api/users/:id/tables/ - добавить новую таблицу <Table>
-
-[GET] /api/users/:id/tables/:table_id/ - таблица по table ID <Table>
-[PATCH] /api/users/:id/tables/:table_id/ - изменение параметров таблицы
-[DELETE] /api/users/:id/tables/:table_id/ - удалить таблицу
-
-[POST] /api/users/:id/tables/:table_id/columns/ - добавить новую колонку <Column>
-
-[PATCH] /api/users/:id/tables/:table_id/columns/:column_id/ - изменение параметров колонки
-[DELETE] /api/users/:id/tables/:table_id/columns/:column_id/ - удалить колонку
-
-[POST] /api/users/:id/tables/:table_id/columns/:column_id/tasks/ - добавить нувую задачу <Task>
-
-[GET] /api/users/:id/tables/:table_id/columns/:column_id/tasks/:task_id/ - задача по ID <Task>
-[PATCH] /api/users/:id/tables/:table_id/columns/:column_id/tasks/:task_id/ - изменение параметров задачи
-[DELETE] /api/users/:id/tables/:table_id/columns/:column_id/tasks/:task_id/ - удаленить задачу
-*/
