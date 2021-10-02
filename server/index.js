d4c63165474198ba156f445b232571d14d5dcf88
K1nGsmaN-hub <15162342h@gmail.com>
2021-10-02 19:34:57 +0800

    feat: connect routes to app server

--- server/index.js
+++ server/index.js
@@ -3,13 +3,22 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const URLS = require('./api/urls');
+const userRoutes = require('./api/routes/user.routes');
+const tableRoutes = require('./api/routes/table.route');
+const columnRoutes = require('./api/routes/column.route');
+const taskRoutes = require('./api/routes/task.route');
+
 const app = express();
 const { BACK_PORT, DB_URI } = process.env;
 const PORT = parseInt(BACK_PORT, 10) || 3001;
 
 mongoose.connect(
     DB_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
     (error) => {
         if (error) throw error;
 
@@ -19,6 +28,11 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+
+app.use(URLS.USERS, userRoutes);
+app.use(URLS.TABLES, tableRoutes);
+app.use(URLS.COLUMNS, columnRoutes);
+app.use(URLS.TASKS, taskRoutes);
 
 app.listen(PORT, (err) => {
     if (err) throw new Error(err);
