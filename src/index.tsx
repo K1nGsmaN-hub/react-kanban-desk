b6a191f3e1fedd04a5b8813caa6cee7edc279efb
K1nGsmaN-hub <15162342h@gmail.com>
2021-10-04 19:10:06 +0800

    feat: create redux store and connect it to entry client file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+
+import App from './app/app';
+import store from './store';
+
 import './index.css';
 
-import App from './app';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
